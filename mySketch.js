ac474a25e4c412a34450a5acaf0b3bc2465230bd
Hsuan911026 <106651059+Hsuan911026@users.noreply.github.com>
2022-06-01 15:26:43 +0800

    Add files via upload

--- mySketch.js
+++ mySketch.js
@@ -1,68 +1,38 @@
+var colors = "064789-427aa1-ebf2fa-679436-a5be00".split("-").map(a=>"#"+a)
+class Ball{
+ constructor(){//預設值
+  this.r=random(150)
+  this.p={x:random(width),y:random(height)}
+  this.v={x:random(-1,1),y:random(-1,1)}
+  this.color=random(colors)
+ }
+ 
+ }
+
+var ball
+var balls=[]
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background("#ffe5d9");
+ createCanvas(windowWidth, windowHeight);
+ background(100);
+ for(var i=0;i<120;i++){
+ ball=new Ball//產生一個新的ball元件
+ balls.push(ball)
 }
-function drawflower(clr,size=1) {
-	push()
-    fill("#B373B3")
-		ellipse(0, 0, 50);		//目前此圓，仍以圓心為座標點
-	if(clr){
-		 fill(clr)
-	}
-	else
-	{
-		fill(255,90,61)
-	}
-		ellipseMode(CORNER)   // 設定以左上角為座標點上的座標
-	
-	for(var i=0;i<16;i++){
-		ellipse(30, -15, 100*size, 30);  // 設定以左上角為座標點，做一個花瓣
-		line(35, -5, 120*size,-5)
-		rotate(PI/8)
-	}
-	pop()
-	
-	
-	
 }
-var colors=["#9c89b8","#f0a6ca","#efc3e6","#f0e6ef","#b8bedd"]
 
 function draw() {
-	background("#ffe5d9")
-	var count=0
-	for(i=0;i<height;i=i+150)
-	{
-		for(j=0;j<width;j=j+150)
-			
-	{
-		count=count+1
-	push()
-	translate(j,i)
-	scale(0.8)
-	rotate(frameCount/100)
-	drawflower(colors[count%colors.length],map(mouseX,0,width,0,1))
-	pop()
-
+ background(100);
+ for(var i=0;i<balls.length;i++){
+ let ball =balls[i]
+  fill(ball.color)
+  circle(ball.p.x ,ball.p.y ,ball.r);
+  ball.p.x+=ball.v.x
+  ball.p.y+=ball.v.y
+ 
+ 
+ 
+ 
+ 
+ 
+ }
 }
-}
-}
-
-
-/*
-push()
-	translate(0,0)
-	drawflower("#E67399",map(mouseX,0,width,0,2))
-	pop()
-	push()
-	translate(300,0); 
-	drawflower()
-	pop()
-	push()
-	translate(0,600);
-	drawflower()
-	pop()
-	push()
-	translate(-300,0);
-	drawflower("#C4D1F7")
-	pop()
-	*/
